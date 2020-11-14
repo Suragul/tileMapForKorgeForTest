1f3ff175ae531bd1be8c0ed89e39a6a7d0251680
Suragul <71817958+Suragul@users.noreply.github.com>
2020-11-14 22:52:04 +0500

    Delete 1.tsx

--- 1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="1" tilewidth="32" tileheight="16" tilecount="108" columns="9">
- <image source="jpisotbs.png" width="300" height="200"/>
-</tileset>
